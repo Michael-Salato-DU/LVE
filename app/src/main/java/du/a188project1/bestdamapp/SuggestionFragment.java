de39ba6b4742278eacc19dfcae320a690fd66d3f
Tess <tess.julien@drake.edu>
2018-10-08 23:30:53 -0500

    Added Main Activity Fragments

---
+++ app/src/main/java/du/a188project1/bestdamapp/SuggestionFragment.java
@@ -0,0 +1,35 @@
+/*
+Tess Julien
+October 8, 2018
+Fragment that will hold a list of suggested events
+ */
+package du.a188project1.bestdamapp;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SuggestionFragment extends Fragment {
+
+
+    public SuggestionFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_suggestion, container, false);
+        return view;
+    }
+
+}
