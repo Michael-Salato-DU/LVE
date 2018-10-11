94fe5b09c18d084bd75d444c10bd24da1325665a
Tess <tess.julien@drake.edu>
2018-10-11 10:14:13 -0500

    Recycler View Suggestion Tab

---
+++ app/src/main/java/du/a188project1/bestdamapp/SuggestionAdapter.java
@@ -0,0 +1,63 @@
+package du.a188project1.bestdamapp;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class SuggestionAdapter extends RecyclerView.Adapter<SuggestionAdapter.SuggestionViewHolder>{
+
+    private Context context;
+    private ArrayList<Event> events;
+    private RecyclerViewClickListener mListener;
+
+    public SuggestionAdapter(Context context, ArrayList<Event> dataSet, RecyclerViewClickListener clickListener) {
+        this.context = context;
+        this.events = dataSet;
+        this.mListener = clickListener;
+    }
+
+    public static class SuggestionViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public TextView performerView;
+        public TextView venueView;
+        public TextView dateView;
+        private RecyclerViewClickListener mListener;
+        public SuggestionViewHolder(View v, RecyclerViewClickListener listener){
+            super(v);
+            performerView = v.findViewById(R.id.performer_view);
+            venueView = v.findViewById(R.id.venue_view);
+            dateView = v.findViewById(R.id.date_view);
+            mListener = listener;
+            v.setOnClickListener(this);
+
+        }
+
+        @Override
+        public void onClick(View view) {
+            mListener.onClick(view, getAdapterPosition());
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return events.size();
+    }
+
+    @Override
+    public SuggestionAdapter.SuggestionViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+        View v = (View) LayoutInflater.from(parent.getContext()).inflate(R.layout.suggestion_cell, parent, false);
+        SuggestionViewHolder vh = new SuggestionViewHolder(v, mListener);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(SuggestionViewHolder holder, int position){
+        holder.performerView.setText(events.get(position).getPerformer().getName());
+        holder.dateView.setText(events.get(position).getDate());
+        holder.venueView.setText(events.get(position).getVenue().getVenueName());
+    }
+}
--- app/src/main/java/du/a188project1/bestdamapp/SuggestionFragment.java
+++ app/src/main/java/du/a188project1/bestdamapp/SuggestionFragment.java
@@ -6,11 +6,18 @@ Fragment that will hold a list of suggested events
 package du.a188project1.bestdamapp;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.io.Serializable;
+import java.util.ArrayList;
 
 
 /**
@@ -18,6 +25,9 @@ import android.view.ViewGroup;
  */
 public class SuggestionFragment extends Fragment {
 
+    private RecyclerView suggestionList;
+    private RecyclerView.LayoutManager layoutManager;
+    private RecyclerView.Adapter suggestionAdapter;
 
     public SuggestionFragment() {
         // Required empty public constructor
@@ -29,6 +39,47 @@ public class SuggestionFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_suggestion, container, false);
+
+        ArrayList<Event> suggestedEvents = new ArrayList<Event>();
+        suggestionList = (RecyclerView)view.findViewById(R.id.suggestion_list);
+
+        Event event1 = new Event();
+        Band CoryOren = new Band();
+        Venue Vaudeville = new Venue();
+        Vaudeville.setVenueName("Vaudeville");
+        CoryOren.setName("Cory Oren");
+        event1.setPerformer(CoryOren);
+        event1.setDate("10/30/2018");
+        event1.setVenue(Vaudeville);
+
+        Event event2 = new Event();
+        Band BandPerry = new Band();
+        Venue Woolys = new Venue();
+        BandPerry.setName("The Band Perry");
+        Woolys.setVenueName("Woolys");
+        event2.setPerformer(BandPerry);
+        event2.setDate("11/3/2018");
+        event2.setVenue(Woolys);
+
+        suggestedEvents.add(event1);
+        suggestedEvents.add(event2);
+
+        layoutManager = new LinearLayoutManager(getContext());
+        suggestionList.setLayoutManager(layoutManager);
+
+        RecyclerViewClickListener listener = new RecyclerViewClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                Event event = (Event) suggestedEvents.get(position);
+                Intent intent = new Intent(view.getContext(), EventActivity.class);
+                intent.putExtra("event", (Serializable)event);
+                startActivity(intent);
+            }
+        };
+
+        suggestionAdapter = new SuggestionAdapter(getContext(), suggestedEvents, listener);
+        suggestionList.setAdapter(suggestionAdapter);
+
         return view;
     }
 
